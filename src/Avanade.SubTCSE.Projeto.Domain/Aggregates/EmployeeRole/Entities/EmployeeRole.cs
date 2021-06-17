55081b09b4df5a104fd60a879ebd1e09a2e217b4
Marcio Coelho Socorro <marcioc@gmail.com>
2021-06-17 09:00:08 -0300

    Aula 4

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,19 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public string Id { get; init; }
+
+        public string RoleName { get; init; }
+    }
+}
