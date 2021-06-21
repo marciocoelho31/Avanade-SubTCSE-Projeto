7dd66e2fb7901703d2ff2651f3332b9c8de1045f
Marcio Coelho Socorro <marcioc@gmail.com>
2021-06-21 08:59:39 -0300

    Aula 21-06-2021

---
+++ src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+        public EmployeeRoleProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDTO, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("RoleName", options => options.MapFrom(src => src.Cargo));
+            CreateMap<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, Dtos.EmployeeRole.EmployeeRoleDTO>()
+                .ForMember("Cargo", memberOptions => memberOptions.MapFrom(src => src.RoleName));
+        }
+    }
+}
